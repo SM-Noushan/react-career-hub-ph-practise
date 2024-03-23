73da1c9997526f6dae63012d38c75cad1e9d6c14
SMNoushan <noushan.md@gmail.com>
2024-03-23 16:33:58 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
 
 function App() {
   return (
     <>
-      <Outlet />
+      <div className="md:container mx-auto">
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
     </>
   )
